44e20ae5480b124b6562d46dce43c99a0dfc52f1
Julian Pollak <poljpocket@gmail.com>
2023-05-17 00:09:10 +0200

    simplify the code by a big amount

--- main.js
+++ main.js
@@ -11,34 +11,17 @@ $(window).on('load', () => {
 
       // do we even need to scroll capture?
       if (contentWidth > wrapperWidth) {
-        const wrapperHeight = $wrapper.outerHeight();
+        // the amount to scroll horizontally
+        const scrollMaximum = contentWidth - wrapperWidth;
+        // the capture offset from the top of the viewport
+        const scrollOffset = window.scrollY - $capture.offset().top;
 
         // set capture element total height to content width
-        const captureHeight = contentWidth - wrapperWidth;
-        $capture.css('height', captureHeight + 'px');
+        $capture.css('height', contentWidth + 'px');
 
-        const captureOffsetTop = $capture.offset().top;
-        const scrollPosition = window.scrollY;
-
-        // the capture offset from the top of the viewport
-        const topOffset = scrollPosition - captureOffsetTop;
-
-        // the capture offset from the bottom of the viewport
-        const bottomOffset = (captureHeight + captureOffsetTop - wrapperHeight - scrollPosition);
-
-        // are we anywhere below the capture top?
-        const belowTop = topOffset > 0;
-        //  are we anywhere above the capture bottom?
-        const aboveBottom = bottomOffset > 0;
-
-        if (belowTop && aboveBottom) {
-          // we need to add a fraction of the wrapper height so that once we are at the bottom, we are also all the way at the right
-          const wrapperWidthFraction = wrapperHeight * (topOffset / (captureHeight - wrapperHeight));
-          $content.css('--scroll', topOffset + wrapperWidthFraction + 'px');
-        } else if (!belowTop)
-          $content.css('--scroll', '0px');
-        else if (!aboveBottom)
-          $content.css('--scroll', captureHeight + 'px');
+        // clip the scrollOffset between 0 and scrollMaximum and apply
+        $content.css('--scroll',
+            Math.max(0, Math.min(scrollOffset, scrollMaximum)) + 'px');
       }
     };
 
